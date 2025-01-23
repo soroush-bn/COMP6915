02482a0bd377e219f1fe3e64bebf1475740593bd
Ramyarzarza <ramyar.zarza@gmail.com>
2025-01-23 19:56:13 -0330

    Create assign_1_group_14.py

---
+++ assign_1_group_14.py
@@ -0,0 +1,36 @@
+#
+#  Assignment 1
+#
+#  Group 14:
+#  Ramyar Zarza rzarza@mun.ca
+#  <Group Member 2 name> <Group Member 1 email>
+#  <Group Member 3 name> <Group Member 1 email>
+
+####################################################################################
+# Imports
+####################################################################################
+import sys
+import os
+
+#####################################################################################
+#  Implementation - helper functions and question wise result generators
+#####################################################################################
+def classify():
+    print('Performing classification...')
+
+def Q1_results():
+    print('Generating results for Q1...')
+
+def Q2_results():
+    print('Generating results for Q2...')
+
+def Q3_results():
+    print('Generating results for Q3...')
+
+#########################################################################################
+# Calls to generate the results
+#########################################################################################
+if __name__ == "__main__":
+    Q1_results()
+    Q2_results()
+    Q3_results()
