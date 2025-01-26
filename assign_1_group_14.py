8a95f5695a7b2ff9f2c2858be1c1944559a1efc9
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-01-26 13:18:36 -0330

    Adding: noise removal, normalization, polynomial features

--- assign_1_group_14.py
+++ assign_1_group_14.py
@@ -11,10 +11,16 @@
 ####################################################################################
 import sys
 import os
-
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import DBSCAN
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,RobustScaler,PolynomialFeatures
 
 #####################################################################################
 #  Implementation - helper functions and question wise result generators
@@ -30,11 +36,41 @@ class KNN():
         self.best_classifier_k = None
         self.knn_clasifier = None
 
-    def train_test(self, k, train_data, test_data):
+    def train_test(self, k, train_data, test_data,augment=False,normalize = False,noise=False):
         self.knn_clasifier = KNeighborsClassifier(k, p=self.distance_metric)
-        self.knn_clasifier.fit(train_data[['X1', 'X2']], train_data['label'])
-        train_acc = self.knn_clasifier.score(train_data[['X1', 'X2']], train_data['label'])
-        test_acc = self.knn_clasifier.score(test_data[['X1', 'X2']], test_data['label'])
+
+        if noise:
+            train_data = noise_removal(train_data)
+        # print(len(train_data))
+        if augment:
+            poly = PolynomialFeatures(degree=2, include_bias=False)
+            train_labels = train_data['label']
+            test_labels = test_data['label']
+
+            augmented_features = poly.fit_transform(train_data[['X1','X2']])
+            feature_names = poly.get_feature_names_out(input_features=['X1', 'X2'])
+            train_data = pd.DataFrame(augmented_features, columns=feature_names)
+            train_data['label'] = train_labels
+            augmented_features_test = poly.fit_transform(test_data[['X1','X2']])
+            feature_names_test = poly.get_feature_names_out(input_features=['X1', 'X2'])
+            test_data = pd.DataFrame(augmented_features_test, columns=feature_names_test)
+            test_data['label'] = test_labels
+            features = [[*feature_names]]
+
+
+        else:
+            features = [['X1','X2']]
+
+        if normalize:
+            scaler = MinMaxScaler()
+            for f in features: 
+                train_data[f] = scaler.fit_transform(train_data[f])
+                test_data[f] = scaler.fit_transform(test_data[f])
+            # print(train_data.describe())
+            
+        self.knn_clasifier.fit(train_data[features[0]], train_data['label'])
+        train_acc = self.knn_clasifier.score(train_data[features[0]], train_data['label'])
+        test_acc = self.knn_clasifier.score(test_data[features[0]], test_data['label'])
         
         if self.best_classifier == None:
             self.best_classifier = self.knn_clasifier
@@ -48,8 +84,11 @@ class KNN():
 
         return train_acc, test_acc
     
-    def generate_grid(self, grid):
-        return self.knn_clasifier.predict(grid[['X1', 'X2']])
+    def generate_grid(self, grid,augment= False):
+        if augment :
+            return self.knn_clasifier.predict(grid[['X1', 'X2', 'X1^2', 'X1 X2', 'X2^2']])
+        else:
+            return self.knn_clasifier.predict(grid[['X1', 'X2']])
 
 
 def data_loader(positive_dir, negative_dir):
@@ -66,8 +105,17 @@ def train_data_loader():
 def test_data_loader():
     return data_loader('test.sDAT.csv', 'test.sNC.csv')
 
-def grid_point_loader():
-    return pd.read_csv('2D_grid_points.csv', names=['X1', 'X2'], header=None)
+def grid_point_loader(augment=False):
+    df=  pd.read_csv('2D_grid_points.csv', names=['X1', 'X2'], header=None)
+    if augment:
+        df_mean_x1 =  np.mean(df['X1'].values)
+        df_mean_x2 =  np.mean(df['X2'].values)
+        poly = PolynomialFeatures(degree=2, include_bias=False)
+        augmented_features = poly.fit_transform(df[['X1','X2']])
+        feature_names = poly.get_feature_names_out(input_features=['X1', 'X2'])
+        df= pd.DataFrame(augmented_features, columns=feature_names)
+    # df['X3'] = (df['X1']-df_mean_x1)**2  + ( df['X2']-df_mean_x2)**2
+    return df
         
 def plot(train, test, grid, class_boundary, title):
     train_label = train['label'].astype(str)
@@ -110,8 +158,11 @@ def Q1_results():
         train_accs.append(train_acc)
         title = f'k={k} - Train Error = {round(1-train_acc, 2)} - Test Error = {round(1-test_acc, 2)} - distance= Euclidean'
         plot(train, test, grid, class_boundary, title)
+    print("--"*5 +"Question 1" + "--" *5)
+
     print(f'test accuracies : {test_accs}')
     print(f'train accuracies : {train_accs}')
+    print("--"*10)
 
     plt.plot(ks, test_accs, 'g--', label="Test Error Rate")
     plt.plot(ks, train_accs, 'b--', label="Train Error Rate")
@@ -133,8 +184,10 @@ def Q2_results():
     train_acc, test_acc, class_boundary = *knn_Manhattan.train_test(best_k, train, test), knn_Manhattan.generate_grid(grid)
     title = f'k={best_k} - Train Error = {round(1-train_acc, 2)} - Test Error = {round(1-test_acc, 2)} - distance= Manhattan'
     plot(train, test, grid, class_boundary, title)
+    print("--"*5 +"Question 2" + "--" *5)
     print(f'Euclidean -> Best Accuracy = {knn_Euclidean.best_classifier_accuracy} and Best k = {knn_Euclidean.best_classifier_k}')
     print(f'Manhattan -> Best Accuracy = {knn_Manhattan.best_classifier_accuracy} and Best k = {knn_Manhattan.best_classifier_k}')
+    print("--"*10)
 
 def Q3_results():
     train = train_data_loader()
@@ -161,7 +214,40 @@ def Q3_results():
     plt.ylabel('Error Rate')
     plt.legend(loc='lower left')
     plt.show()
-        
+    print("--"*5 +"Question 3" + "--" *5)
+    print(f'Q3 finished')
+    print("--"*10)
+
+def noise_removal(df):
+    # print(df.describe())
+    dbscan = DBSCAN(eps=0.1, min_samples=4)  
+    df['cluster'] = dbscan.fit_predict(df[['X1','X2']])
+    refined_df = df[df['cluster'] != -1].drop(columns=['cluster'])  
+    
+    return refined_df 
+
+def Q4_results():
+    global knn_Euclidean
+
+    train = train_data_loader()
+    test = test_data_loader()
+    grid = grid_point_loader()
+
+    knn_Euclidean = KNN('Euclidean')
+    ks = [30]
+    # ks = [i for i in range(1,len(train),10)]
+    test_accs=  [] 
+    train_accs = [] 
+    for k in ks:
+        train_acc, test_acc, class_boundary = *knn_Euclidean.train_test(k, train, test,normalize=True,noise=True), knn_Euclidean.generate_grid(grid)
+        test_accs.append(test_acc)
+        train_accs.append(train_acc)
+        title = f'k={k} - Train Error = {round(1-train_acc, 2)} - Test Error = {round(1-test_acc, 2)} - distance= Euclidean'
+        plot(train, test, grid, class_boundary, title)
+    print("Question 4 accuracy:")
+    print(f"test_acc:{test_accs},train_accuracy:{train_accs}")
+
+    
 
 #########################################################################################
 # Calls to generate the results
@@ -170,3 +256,4 @@ if __name__ == "__main__":
     Q1_results()
     Q2_results()
     Q3_results()
+    Q4_results()
